03c7fb03db2419c691ae87a16d3d5194e95dc287
Chesium <68589881+Chesium@users.noreply.github.com>
2024-09-04 14:35:27 +0000

    ants: pass P4

--- w5/ants/ants.py
+++ w5/ants/ants.py
@@ -166,6 +166,8 @@ class ThrowerAnt(Ant):
     damage = 1
     # ADD/OVERRIDE CLASS ATTRIBUTES HERE
     food_cost=3
+    lower_bound = 0
+    upper_bound = float('inf')
 
     def nearest_bee(self):
         """Return the nearest Bee in a Place (that is not the hive) connected to
@@ -174,12 +176,20 @@ class ThrowerAnt(Ant):
         This method returns None if there is no such Bee (or none in range).
         """
         # BEGIN Problem 3 and 4
+        ## short : 0-3  long : 5-inf
         cur_place=self.place
+        step=0
         while 1:
-            if cur_place.bees == []:
+            if step>self.upper_bound:
+                return None
+            elif cur_place.bees == []:
                 if cur_place.entrance.is_hive:
                     return None
                 cur_place=cur_place.entrance
+                step+=1
+            elif step<self.lower_bound:
+                cur_place=cur_place.entrance
+                step+=1
             else:
                 break
         return random_bee(cur_place.bees)
@@ -213,8 +223,9 @@ class ShortThrower(ThrowerAnt):
     name = 'Short'
     food_cost = 2
     # OVERRIDE CLASS ATTRIBUTES HERE
+    upper_bound = 3
     # BEGIN Problem 4
-    implemented = False   # Change to True to view in the GUI
+    implemented = True   # Change to True to view in the GUI
     # END Problem 4
 
 
@@ -224,8 +235,9 @@ class LongThrower(ThrowerAnt):
     name = 'Long'
     food_cost = 2
     # OVERRIDE CLASS ATTRIBUTES HERE
+    lower_bound = 5
     # BEGIN Problem 4
-    implemented = False   # Change to True to view in the GUI
+    implemented = True   # Change to True to view in the GUI
     # END Problem 4
 
 
