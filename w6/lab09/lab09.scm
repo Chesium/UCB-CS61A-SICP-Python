b5be43ceab0ce84066421e7c14d6b1f0af9da967
Chesium <68589881+Chesium@users.noreply.github.com>
2024-09-29 20:50:53 +0000

    complete lab09

--- w6/lab09/lab09.scm
+++ w6/lab09/lab09.scm
@@ -1,10 +1,19 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) (
+  if (< num1 num2) 
+    -1 
+    (if (= num1 num2)
+      0
+      1
+)))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) (lambda (inc) (+ num inc)))
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) (lambda (x) (f (g x))))
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n) (if (= n 1)
+  f
+  (composed (repeat f (- n 1)) f))
+)
 
 (define (max a b)
   (if (> a b)
@@ -16,12 +25,32 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b) (begin 
+  (define mi (min a b))
+  (define mx (max a b))
+  (if (= (modulo mx mi) 0)
+    mi
+    (gcd mi (modulo mx mi))
+  )
+))
 
-(define (duplicate lst) 'YOUR-CODE-HERE)
+(define (duplicate lst) 
+  (if (null? lst)
+    `()
+    (cons (car lst) (cons (car lst) (duplicate (cdr lst))))
+  )
+)
 
 (expect (duplicate '(1 2 3)) (1 1 2 2 3 3))
 
 (expect (duplicate '(1 1)) (1 1 1 1))
 
-(define (deep-map fn s) 'YOUR-CODE-HERE)
+(define (deep-map fn s) 
+  (if (null? s)
+    `()
+    (if (list? (car s))
+      (cons (deep-map fn (car s)) (deep-map fn (cdr s)))
+      (cons (fn (car s)) (deep-map fn (cdr s)))
+    )
+  )
+)
