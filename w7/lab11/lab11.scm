a59917f1198f8f2587b516cb4609e384ca8fd584
Chesium <68589881+Chesium@users.noreply.github.com>
2024-10-04 09:09:33 +0000

    complete lab11 scheme part

--- w7/lab11/lab11.scm
+++ w7/lab11/lab11.scm
@@ -1,23 +1,46 @@
 (define (cadr lst) (car (cdr lst)))
 
 (define (make-kwlist1 keys values)
-  'YOUR-CODE-HERE)
+  (list keys values))
 
-(define (get-keys-kwlist1 kwlist) 'YOUR-CODE-HERE)
+(define (get-keys-kwlist1 kwlist) (car kwlist))
 
 (define (get-values-kwlist1 kwlist)
-  'YOUR-CODE-HERE)
+  (cadr kwlist))
 
 (define (make-kwlist2 keys values)
-  'YOUR-CODE-HERE)
+  (if (null? keys)
+    nil
+    (cons (list (car keys) (car values)) (make-kwlist2 (cdr keys) (cdr values)))
+  ))
 
-(define (get-keys-kwlist2 kwlist) 'YOUR-CODE-HERE)
+(define (get-keys-kwlist2 kwlist)
+  (if (null? kwlist)
+    nil
+    (cons (car (car kwlist)) (get-keys-kwlist2 (cdr kwlist)))
+  ))
 
 (define (get-values-kwlist2 kwlist)
-  'YOUR-CODE-HERE)
+  (if (null? kwlist)
+    nil
+    (cons (cadr (car kwlist)) (get-values-kwlist2 (cdr kwlist)))
+  ))
+
 
 (define (add-to-kwlist kwlist key value)
-  'YOUR-CODE-HERE)
+  (make-kwlist (append (get-keys-kwlist kwlist) (list key)) (append (get-values-kwlist kwlist) (list value))))
 
 (define (get-first-from-kwlist kwlist key)
-  'YOUR-CODE-HERE)
+    (let ((k (get-keys-kwlist kwlist)) (v (get-values-kwlist kwlist)))
+      (if (null? k)
+        nil
+        (if (eq? (car k) key)
+          (car v)
+          ; (car v)
+          ; (get-first-from-kwlist (make-kwlist '(a b c) '(1 2 3)) 'a)
+          (get-first-from-kwlist (make-kwlist (cdr k) (cdr v)) key)
+        )
+      )
+    )
+)
+
